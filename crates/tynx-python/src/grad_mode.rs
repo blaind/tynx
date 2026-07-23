d987f4bd9c5ee6813829ad023723de090498d462
Mika <blaind@blaind.net>
2026-07-23 11:40:48 +0300

    Support no_grad as a decorator (#69)

--- crates/tynx-python/src/grad_mode.rs
+++ crates/tynx-python/src/grad_mode.rs
@@ -2,7 +2,11 @@
 
 use std::cell::Cell;
 
-use pyo3::{exceptions::PyRuntimeError, prelude::*, types::PyAny};
+use pyo3::{
+    exceptions::PyRuntimeError,
+    prelude::*,
+    types::{PyAny, PyDict, PyTuple},
+};
 
 thread_local! {
     static GRAD_ENABLED: Cell<bool> = const { Cell::new(true) };
@@ -43,6 +47,59 @@ impl PyNoGrad {
         self.restore();
         false
     }
+
+    fn __call__(&self, py: Python<'_>, function: Py<PyAny>) -> PyResult<Py<PyAny>> {
+        no_grad_wrapper(py, function)
+    }
+}
+
+/// Callable wrapper produced when `no_grad()` is used as a decorator.
+#[pyclass(name = "_NoGradFunction", dict, frozen, unsendable)]
+pub(crate) struct PyNoGradFunction {
+    function: Py<PyAny>,
+}
+
+#[pymethods]
+impl PyNoGradFunction {
+    fn __get__(
+        &self,
+        py: Python<'_>,
+        instance: &Bound<'_, PyAny>,
+        owner: &Bound<'_, PyAny>,
+    ) -> PyResult<Py<PyAny>> {
+        let function = self
+            .function
+            .bind(py)
+            .call_method1("__get__", (instance, owner))?
+            .unbind();
+        no_grad_wrapper(py, function)
+    }
+
+    #[pyo3(signature = (*args, **kwargs))]
+    fn __call__<'py>(
+        &self,
+        py: Python<'py>,
+        args: &Bound<'py, PyTuple>,
+        kwargs: Option<&Bound<'py, PyDict>>,
+    ) -> PyResult<Py<PyAny>> {
+        let previous = replace_grad_enabled(false);
+        let result = self.function.bind(py).call(args, kwargs).map(Bound::unbind);
+        replace_grad_enabled(previous);
+        result
+    }
+}
+
+fn no_grad_wrapper(py: Python<'_>, function: Py<PyAny>) -> PyResult<Py<PyAny>> {
+    let wrapper = Py::new(
+        py,
+        PyNoGradFunction {
+            function: function.clone_ref(py),
+        },
+    )?;
+    py.import("functools")?
+        .getattr("update_wrapper")?
+        .call1((wrapper.clone_ref(py), function))?;
+    Ok(wrapper.into_any())
 }
 
 impl PyNoGrad {
